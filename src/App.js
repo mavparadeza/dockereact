9423108678954e51786bea47921bdd9bac55360b
Mav Paradeza <mav.paradeza@gmail.com>
2021-06-27 16:53:02 +0800

    updated appjs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          What's up?
+          Initiated from feature br.
         </p>
         <a
           className="App-link"
